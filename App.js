342c5acff3c653281fe54d34a141b6f9649690ac
MaximCastornyi <nets.devs@gmail.com>
2020-03-04 13:11:42 +0200

    Photo Gallery App

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './app/components/Header';
+import Mid from './app/components/Mid';
+import Body from './app/components/Body';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Mid />
+      <Body />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
